8749dd8870489771607a7a4f8d9f8f5034e0b660
nqnzp <adamcopeland@protonmail.com>
2018-08-30 16:16:22 -0400

    It's all good

--- google-alias-push-prod.py
+++ google-alias-push-prod.py
@@ -11,32 +11,32 @@ if __name__ == "__main__":
 
     # Setup the Admin SDK Directory API
     SCOPES = 'https://www.googleapis.com/auth/admin.directory.user'
-    store = file.Storage('credentials.json')
+    store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('/home/adam/PycharmProjects/google-alias-push/client_secret-prod.json', SCOPES)
+        flow = client.flow_from_clientsecrets('/home/adam/PycharmProjects/google-alias-push/credentials-prod.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('admin', 'directory_v1', http=creds.authorize(Http()))
 
     # Setup the local LDAP connection
     Config = configparser.ConfigParser(comment_prefixes='#')
-    server = Server('ldap.montclair.edu', get_info=ALL)
     Config.read('./config.ini')
-    basedn = Config.get('doit','basedn')
-    authdn = Config.get('doit','authdn')
-    authpw = Config.get('doit','authpw', raw=True)
+    basedn = Config.get('google-alias-push','basedn')
+    authdn = Config.get('google-alias-push','authdn')
+    authpw = Config.get('google-alias-push','authpw', raw=True)
+    host = Config.get('google-alias-push','host')
 
+    server = Server(host, get_info=ALL)
     conn = Connection(server, user=authdn, password=authpw)
     conn.bind()
     conn.start_tls()
 
-    #debugging
-    #print(conn, '\n')
     print("Authenticated as " + conn.extend.standard.who_am_i(), '\n')
 
     obj_inet = ObjectDef('inetLocalMailRecipient', conn)
     r = Reader(conn, obj_inet, basedn, '!(employeeType=ST)')
     r.search()
+
 
     for entry in r.entries:
 
@@ -46,6 +46,20 @@ if __name__ == "__main__":
             if not (str(alt) == gaccount):
                 print(str(alt) + ' => ' + gaccount)
 
+                alias = str(alt).replace('mail.', '')
+                netid = gaccount
+
+                requestDict = {'alias': alias}
+                request = service.users().aliases()
+
+                try:
+                    if service.users().aliases().insert(userKey=netid, body=requestDict).execute():
+                        results = service.users().aliases().list(userKey=netid).execute()
+                        alias = results.get('aliases', [])
+                        print(alias[0]['alias'])
+                except:
+                    continue
+
         try:
             results = service.users().aliases().list(userKey=gaccount).execute()
         except:
@@ -54,14 +68,4 @@ if __name__ == "__main__":
         alias = results.get('aliases', [])
         for item in alias:
             print(item['alias'])
-
-    # Generate request for insert
-    #requestDict = {'alias': 'adam.copeland@sandbox.montclair.edu'}
-    #request = service.users().aliases()
-
-    #if service.users().aliases().insert(userKey='copelanda@sandbox.montclair.edu', body=requestDict).execute():
-    #    results = service.users().aliases().list(userKey='copelanda@sandbox.montclair.edu').execute()
-    #    alias = results.get('aliases', [])
-    #    print(alias[0]['alias'])
-
 
